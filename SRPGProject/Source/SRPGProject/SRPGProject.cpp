fcef775ebb14b35a34b957f4a80b852af3bd32f7
SeanFowke <seanfowke@hotmail.com>
2020-07-13 01:39:54 -0500

    Comit 1

--- SRPGProject/Source/SRPGProject/SRPGProject.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
-
-#include "SRPGProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SRPGProject, "SRPGProject" );
-
-DEFINE_LOG_CATEGORY(LogSRPGProject)
- 
